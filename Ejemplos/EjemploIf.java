84a0f1d95fc9c9a84838bd8993a3d71cb94e97d5
Henry L. Gómez <henryl.gomez@gmail.com>
2021-06-08 08:16:16 -0500

    Add new examples

---
+++ Ejemplos/EjemploIf.java
@@ -0,0 +1,26 @@
+/* 
+	Este es un comentario de
+	varias líneas
+*/
+
+public class EjemploIf{
+
+
+	public static void main(String[] args){
+		int atendees = 5;
+		boolean isALargeVenue;
+
+		isALargeVenue = atendees >= 10;
+
+		if(isALargeVenue){
+			System.out.println("El salón es grande");
+		}else if(){
+			System.out.println("El salón no es grande");
+		}else if(){
+			
+		}else{
+			
+		}
+	}
+
+}
--- Ejemplos/Main.java
+++ Ejemplos/Main.java
@@ -1,11 +1,18 @@
+/* 
+	Este es un comentario de
+	varias líneas
+*/
+
 public class Main{
+
 
 	public static void main(String[] args){
 		String greetingMessage = "Hello World";
 		String messageEnding = "....";
+		int population = 50_367_424; //50367424  Este es un comentario de una sola línea
 		int num1 = 11;
 		int num2 = 2;
-		double result = num1 / num2;
+		int result = population + num1;
 		System.out.println(result);
 	}
 
