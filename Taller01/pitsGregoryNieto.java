545ef7b9a32d06f6918553cbb8d8cab4364e1437
Jóse Gregory <divisanaranja10@gmail.com>
2021-06-08 07:27:34 -0500

    agregar archivo

---
+++ Taller01/pitsGregoryNieto.java
@@ -0,0 +1,18 @@
+public class pitsGregoryNieto{
+
+	public static void main(String[] args){
+		string mechanic1 = "Quita llanta 1 y pone llanta nueva";
+		string mechanic2 = "Quita llanta 2 y pone llanta nueva";
+		string mechanic3 = "Quita llanta 3 y pone llanta nueva";
+		string mechanic4 = "Quita llanta 4 y pone llanta nueva";		
+		string mechanic5 = "Tanquea el vehiculo";	
+		
+		System.out.println(mechanic1);
+		System.out.println(mechanic2);
+		System.out.println(mechanic3);
+		System.out.println(mechanic4);
+		System.out.println(mechanic5);
+		System.out.println("Todo listo y arranca el vehiculo");
+	}
+
+}
