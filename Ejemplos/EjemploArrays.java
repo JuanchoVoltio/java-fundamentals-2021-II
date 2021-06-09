ae8e58687eff981ca1e8f7e9fbd3a6077b355a69
Henry L. Gómez <henryl.gomez@gmail.com>
2021-06-09 07:22:07 -0500

    Agregar ejemplo de Arreglos

---
+++ Ejemplos/EjemploArrays.java
@@ -0,0 +1,46 @@
+/* 
+	Este es un comentario de
+	varias líneas
+*/
+
+public class EjemploArrays{
+
+
+	public static void main(String[] args){
+		int[] ages = {10, 15, 9, 13};
+		//printArray(ages);
+		
+		ages[0] = 11;
+
+		//printArray(ages);
+		findElement(ages, 15);
+	}
+
+	public static void printArray(int[] array){
+		for(int current : array){ // for current in array:
+			System.out.println(current);	
+		}
+		System.out.println("--------");
+	}
+
+	public static void findElement(int[] array, int element){
+		//int iterations = 0;		
+
+		for(int current : array){
+			//iterations++;
+			if(current == element){
+				System.out.println(element + " founded!");
+				break;
+			}
+		}
+
+		//System.out.println("I made " + iterations + " iterations");
+	}
+
+	//TODO: Agregar una función que indique si hay un número negativo en el arreglo
+
+	public static void findNegative(){
+		
+	}
+
+}
