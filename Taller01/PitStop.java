ce81f0acce84c087a95990c1049ff0066b2775b2
CP. Cenidier Ospina <cenidierospina78@gmail.com>
2021-06-09 00:52:39 -0500

    Ejercicio Pits Ospina

---
+++ Taller01/PitStop.java
@@ -0,0 +1,29 @@
+
+public class PitStop {
+
+    public static void main(String args[]) {
+        System.out.println("*******PITS*********");
+        System.out.println("");
+        System.out.println("La próxima vuelta se debe ingresar a la zona de pits");
+        System.out.println("");
+        System.out.println("¡¡¡¡¡¡¡ALERTA DE APROXIMACIÓN A ZONA DE PITS!!!!!!!");
+        System.out.println("Recuerde que la velocidad máxima en pitsline es 80km/h");
+        System.out.println("****************************************************");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("¡¡¡¡¡¡¡¡¡¡¡ALERTA EQUIPO DE MECÁNICOS!!!!!!!!!!!!");
+        System.out.println("............Vehículo aproximandose................");
+        System.out.println("");
+        System.out.println("*********VEHÍCULO DETENIDO***********");
+        System.out.println("02 Mecánicos deben alzar el vehículo en la parte delantera y trasera");
+        System.out.println("");
+        System.out.println("*********CAMBIO DE NEUMÁTICOS*********");
+        System.out.println("El cambio se realiza con grupos compuetos por 03 mecánicos; 01 grupo por cada rueda");
+        System.out.println("Equipo 1 cambia la rueda derecha trasera");
+        System.out.println("Equipo 2 cambia la rueda  izquierda trasera");
+        System.out.println("Equipo 3 cambia la rueda  derecha delantera");
+        System.out.println("Equipo 4 cambia la rueda  izquierda delantera");
+      
+    }
+
+}
